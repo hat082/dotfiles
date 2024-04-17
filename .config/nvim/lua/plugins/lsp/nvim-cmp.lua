062b98ba50e662c86df8bc7e5d446ed03f7be350
hat082 <2798638863@qq.com>
2024-04-17 16:28:49 +0800

    added templates, rewrote lsp config and added luasnip

--- .config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ .config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -1,69 +1,234 @@
 return {
-	{
-		"luozhiya/fittencode.nvim",
-		config = function()
-			require("fittencode").setup()
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			"saadparwaiz1/cmp_luasnip",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
+  "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
+  dependencies = {
+    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "hrsh7th/cmp-path", -- source for file system paths
+    "L3MON4D3/LuaSnip", -- snippet engine
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    -- "rafamadriz/friendly-snippets", -- useful snippets
+    -- "onsails/lspkind.nvim", -- vs-code like pictograms
+    "hrsh7th/cmp-cmdline",
+    "petertriho/cmp-git",
+    "f3fora/cmp-spell",
+    "micangl/cmp-vimtex",
+    -- "aspeddro/cmp-pandoc.nvim",
+  },
+  config = function()
 
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/nvim-cmp",
-		},
+    -- local check_backspace = function()
+    --   local col = vim.fn.col "." - 1
+    --   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+    -- end
 
-		event = "InsertEnter",
-		config = function()
-			-- Set up nvim-cmp.
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+    -- local vimtex_icons = {
+    --     a = "󰧮",
+    --     B = "󰧮",
+    --     c = "󱓷",
+    --     article = "󰧮",
+    --     book = "",
+    --     incollection = "󱓷",
+    --   }
+    --
+    -- local test_fn = function(name, str)
+    --   if str == nil then
+    --       return ""
+    --   end
+    --   if name then
+    --       return ""
+    --   end
+    --
+    --   local type = ((str:match("^%[(.-)%]")):gsub("%[", "")):gsub("%]", "")
+    --   if vimtex_icons[type] ~= nil then
+    --     logger(string.find(str, "^%[(.-)%]"))
+    --     str = string.gsub(str, "^%[(.-)%]", "[" .. vimtex_icons[type] .. "]")
+    --   end
+    --
+    --   return str
+    -- end
 
-			cmp.setup({
-				snippet = {
-					-- REQUIRED - you must specify a snippet engine
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.scroll_docs(-4),
-					["<C-p>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-					{ name = "path" },
-					{ name = "fittencode" },
-				}),
-			})
-		end,
-	},
+    local cmp = require("cmp")
+
+    local luasnip = require("luasnip")
+
+    -- NOTE: don't use this in supertab
+    -- local has_words_before = function()
+    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+    -- end
+
+    -- local lspkind = require("lspkind") -- goes with lspkind.nvim above
+
+    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    -- require("luasnip.loaders.from_vscode").lazy_load()
+
+    --   פּ ﯟ   some other good icons
+    local kind_icons = {
+      article = "󰧮",
+      book = "",
+      incollection = "󱓷",
+      Function = "󰊕",
+      Constructor = "",
+      Text = "󰦨",
+      Method = "",
+      Field = "󰅪",
+      Variable = "󱃮",
+      Class = "",
+      Interface = "",
+      Module = "",
+      Property = "",
+      Unit = "",
+      Value = "󰚯",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "󰌁",
+      -- Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      -- EnumMember = "",
+      Constant = "󰀫",
+      Struct = "",
+      -- Struct = "",
+      Event = "",
+      Operator = "󰘧",
+      TypeParameter = "",
+    }
+    -- find more here: https://www.nerdfonts.com/cheat-sheet
+
+    cmp.setup({
+      completion = {
+        completeopt = "menu,noselect",
+        -- completeopt = "menuone,preview,noinsert,noselect",
+        keyword_length = 1,
+      },
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        -- ["<C-n>"] = cmp.mapping.complete(), -- show completion suggestions
+        -- ["<C-h>"] = cmp.mapping.abort(), -- close completion window
+        -- ["<C-l>"] = cmp.mapping.confirm({ select = false }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        -- supertab
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          elseif cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+          -- BONEYARD
+          -- if luasnip.expand_or_locally_jumpable() then
+          --   luasnip.expand_or_jump()
+          -- elseif luasnip.locally_jumpable() then
+          --   luasnip.jump()
+          --   cmp.confirm()
+          -- elseif has_words_before() then
+          --  cmp.complete()
+        ["<S-Tab>"] = cmp.mapping(function() -- could be: function(fallback) -- OR: function(delete-two-spaces????)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            -- fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      -- formatting for autocompletion
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          vim_item.kind = string.format("%s", kind_icons[vim_item.kind]) -- Kind icons
+          vim_item.menu = ({
+            -- vimtex = (vim_item.menu ~= nil and vim_item.menu or "[VimTex]"),
+            -- vimtex = test_fn(vim_item.menu, entry.source.name),
+            vimtex = vim_item.menu,
+            luasnip = "[Snippet]",
+            nvim_lsp = "[LSP]",
+            buffer = "[Buffer]",
+            spell = "[Spell]",
+            -- orgmode = "[Org]",
+            -- latex_symbols = "[Symbols]",
+            cmdline = "[CMD]",
+            path = "[Path]",
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
+      -- sources for autocompletion
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- snippets
+        { name = "vimtex" },
+        -- { name = "orgmode" },
+        -- { name = "pandoc" },
+        -- { name = "omni" },
+        { name = "buffer", keyword_length = 3 }, -- text within current buffer
+        { name = "spell",
+          keyword_length = 4,
+          option = {
+              keep_all_entries = false,
+              enable_in_context = function()
+                  return true
+              end
+          },
+        },
+        { name = "path" },
+      }),
+      confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+      view = {
+        entries = 'custom',
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+        -- completion = {
+        --   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        -- },
+        -- documentation = {
+        --   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+        -- },
+      },
+      performance = {
+         trigger_debounce_time = 500,
+         throttle = 550,
+         fetching_timeout = 80,
+      },
+    })
+
+    -- `/` cmdline setup.
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        {name = 'buffer'}
+      }
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        {name = 'path'},
+        {name = 'cmdline'}
+      }
+    })
+
+  end,
 }
